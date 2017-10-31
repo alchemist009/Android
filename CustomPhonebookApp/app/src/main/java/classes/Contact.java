355270fb1ddaa44d2d1d7f3ecad46ae09532e9e5
alchemist009 <alchemist009@gmail.com>
2017-10-30 19:36:53 -0500

    Renamed project

---
+++ CustomPhonebookApp/app/src/main/java/classes/Contact.java
@@ -0,0 +1,152 @@
+/**
+ * Helper class to create objects for contact details and implement custom method like toString(),
+ * compareTo(), and equals() for them.
+ *
+ * @authors: Gunjan Tomer, Koulick Sankar Paul
+ *
+ * NetIDs: gxt160930, ksp160330
+ *
+ * version 1.3: 10/28/2017
+ */
+package classes;
+
+import android.support.annotation.NonNull;
+import java.io.Serializable;
+
+
+public class Contact implements Comparable, Serializable {
+    private String firstName, lastName;
+    private String phoneNumber, emailID;
+
+    private static final String S_FIRST_NAME = "FN:";
+    private static final String S_LAST_NAME = "LN:";
+    private static final String S_PHONE_NUMBER = "PN:";
+    private static final String S_EMAIL_ID = "EI:";
+
+    private Contact(){}
+
+    public Contact(String firstName, String lastName, String phoneNumber, String emailID) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.emailID = emailID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmailID() {
+        return emailID;
+    }
+
+
+    /**
+     *
+     * @authors: Gunjan Tomer
+     *
+     * Method to override the default equals and use it to compare contact objects instead.
+     *
+     * A true value is returned when either the corresponding contact details match or one
+     * of the fields is left empty after modification and then compared to an existing object in
+     * the contacts object list.
+     *
+     * @param obj
+     * @return boolean
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if(!(obj instanceof Contact))
+            return false;
+        Contact c = (Contact) obj;
+        return (firstName.equals(c.firstName) || firstName.equals(""))
+                && (lastName.equals(c.lastName) || lastName.equals(""))
+                && (phoneNumber.equals(c.phoneNumber) || phoneNumber.equals(""))
+                && (emailID.equals(c.emailID) || emailID.equals(""));
+    }
+
+    /**
+     * @authors: Koulick Sankar Paul
+     *
+     * Method to override the default toString operation and use a StringBuilder to create a string
+     * with all the contact details appended separated by whitespace.
+     * @return
+     */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(firstName);
+        if (lastName != null) {
+            sb.append(" ").append(lastName);
+        }
+        if (phoneNumber != null) {
+            sb.append(" (").append(phoneNumber).append(")");
+        }
+        return sb.toString();
+    }
+
+    /**
+     *
+     * @author: Koulick Sankar Paul
+     *
+     * Method to override the default compareTo operation to perform comparisons based on FirstName
+     *
+     * @param o
+     * @return int
+     */
+    @Override
+    public int compareTo(@NonNull Object o) {
+        return this.firstName.compareTo(((Contact) o).firstName);
+    }
+
+    /**
+     * @author: Koulick Sankar Paul
+     *
+     * Method to read a string separated by tabs from the text file and store each field in the
+     * corresponding field in the Contact object.
+     *
+     * @param line
+     * @return Contact object
+     */
+    public static Contact readFromString(String line) {
+        String[] split = line.split("\\t");
+        Contact c = new Contact();
+
+        if (!(split[0].substring(3)).isEmpty() && !split[0].substring(3).equals("null")) {
+            c.firstName = (split[0].substring(3)).trim();
+        }
+        if (!(split[1].substring(3)).isEmpty() && !split[1].substring(3).equals("null")) {
+            c.lastName = (split[1].substring(3)).trim();
+        }
+        if (!(split[2].substring(3)).isEmpty() && !split[2].substring(3).equals("null")) {
+            c.phoneNumber = (split[2].substring(3)).trim();
+        }
+        if (!(split[3].substring(3)).isEmpty() && !split[3].substring(3).equals("null")) {
+            c.emailID = (split[3].substring(3)).trim();
+        }
+
+        return c;
+    }
+
+    /**
+     * @author: Gunjan Tomer
+     *
+     * Method to get a string separated by tabs ready to be written to the text file.
+     *
+     * @return String
+     */
+    public String getWritableString() {
+        return S_FIRST_NAME + firstName + "\t"
+                + S_LAST_NAME + lastName + "\t"
+                + S_PHONE_NUMBER + phoneNumber + "\t"
+                + S_EMAIL_ID + emailID;
+    }
+}
---
+++ CustomPhonebookApp/app/src/main/java/com/example/wra1th/customphonebook/MainActivity.java
@@ -0,0 +1,140 @@
+/**
+ * Custom Contact Manager app for  CS6326.001, Assignment 4
+ *
+ * @authors: Koulick Sankar Paul, Gunjan Tomer
+ *
+ * NetIDs: ksp160330, gxt160930
+ *
+ * version: 1.3: 10/28/2017
+ *
+ * This app serves as a rudimentary replacement for the regular contact manager app found on android phones.
+ * The app uses two activities. The first activity is the main screen displaying the list of contacts read from
+ * a text file stored in internal memory. On the top of the screen, in the action bar an icon is present to allow
+ * addition of a new contact. If no contacts have previously been added a new file is created when the
+ * activity is launched. On this activity screen the user has two options: either tap the add icon in the action bar
+ * or select one of the existing contacts and modify or delete it.
+ *
+ * Tapping the add icon takes the user to the next activity, ContactDetailsActivity which shows four fields for the
+ * contact details - FirstName, LastName, PhoneNumber and EmailID  - along with the Save button. The user must enter
+ * at least the FirstName before being allowed to save. Once Save is clicked, the Contact object is passed to
+ * writeContact, sorted in alphabetical order along with the existing contacts and then written to the text file.
+ *
+ * Tapping an item in the listView takes the user to the same activity as the Save action but with the fields populated
+ * with the details from the clicked list item and the Modify and Delete buttons visible. The underlying functionality to add,
+ * delete and modify contacts is defined in FileHandler.java.
+ *
+ * The user can exit the activity and therefore the app anytime by pressing the back key in the android navigation bar.
+ *
+ */
+
+
+
+
+package com.example.wra1th.customphonebook;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.view.View;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import classes.Contact;
+import classes.FileHandler;
+
+public class MainActivity extends AppCompatActivity implements Serializable {
+
+    ListView contactList;
+
+    /**
+     * @author: Koulick Sankar Paul
+     *
+     * onCreate method displays list of contacts on main activity screen
+     *
+     * @param savedInstanceState
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        contactList = (ListView) findViewById(R.id.contact_list);
+        try {
+            FileHandler.createFileIfNotPresent(MainActivity.this);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * @author: Koulick Sankar Paul
+     *
+     * Method to handle a click on one of the contacts in the list
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        List<Contact> list;
+        try {
+            list = FileHandler.readContacts(MainActivity.this);
+            contactList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Contact contact = (Contact) contactList.getItemAtPosition(position);
+                    Intent intent = new Intent(MainActivity.this, ContactDetailsActivity.class);
+                    intent.putExtra(ContactDetailsActivity.OPERATION, ContactDetailsActivity.MODIFY_OPERATION);
+                    intent.putExtra(ContactDetailsActivity.CONTACT, contact);
+                    intent.putExtra(ContactDetailsActivity.POSITION, position);
+                    startActivity(intent);
+                }
+            });
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            list = new ArrayList<>();
+        }
+        ArrayAdapter<Contact> contactArrayAdapter = new ArrayAdapter<>(
+                MainActivity.this, android.R.layout.simple_list_item_1, list);
+        contactList.setAdapter(contactArrayAdapter);
+    }
+
+    /**
+     * @author: Gunjan Tomer
+     *
+     * Method to create the operations available in the action bar
+     * @param menu
+     * @return boolean
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    /**
+     * @author: Gunjan Tomer
+     *
+     * Method to perform add contact operation when add icon clicked in the action bar
+     * @param item
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.add_contact) {
+            Intent intent = new Intent(MainActivity.this, ContactDetailsActivity.class);
+            intent.putExtra(ContactDetailsActivity.OPERATION, ContactDetailsActivity.ADD_OPERATION);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
