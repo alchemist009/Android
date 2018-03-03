91c3f77acebbd3d83d1325f33a7e072c67c87194
alchemist009 <alchemist009@gmail.com>
2018-03-02 20:37:40 -0600

    Added animations to recycler view

--- RecyclerViewDemo/app/src/main/java/com/example/wra1th/recyclerviewdemo/UserRecyclerActivity.java
+++ RecyclerViewDemo/app/src/main/java/com/example/wra1th/recyclerviewdemo/UserRecyclerActivity.java
@@ -2,14 +2,20 @@ package com.example.wra1th.recyclerviewdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.animation.AlphaAnimation;
 
 import com.example.wra1th.recyclerviewdemo.adapters.UserRecyclerAdapter;
 import com.example.wra1th.recyclerviewdemo.models.User;
 
 import java.util.List;
+
+import jp.wasabeef.recyclerview.adapters.AlphaInAnimationAdapter;
+import jp.wasabeef.recyclerview.adapters.ScaleInAnimationAdapter;
 
 public class UserRecyclerActivity extends AppCompatActivity {
 
@@ -29,11 +35,26 @@ public class UserRecyclerActivity extends AppCompatActivity {
 
     private void attachRVandAdapter() {
 
-        recyclerView.setAdapter(userRecyclerAdapter);
+        ScaleInAnimationAdapter animationAdapter = new ScaleInAnimationAdapter(userRecyclerAdapter);
+
+        recyclerView.setAdapter(animationAdapter);
     }
 
     private void prepareRecyclerView() {
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        //For vertical linear layout
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+
+        //For horizontal linear layout
+        //linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+
+        //For grid view in linear layout
+        //linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+
+        //For staggered layout
+        //StaggeredGridLayoutManager staggeredGridLayoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+
+        //GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 4);
+        //recyclerView.setLayoutManager(staggeredGridLayoutManager);
 
         recyclerView.setLayoutManager(linearLayoutManager);
     }
